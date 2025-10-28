56934f532c611dab68b515706281cbc382e0247f
haticegit <haticegedik352@gmail.com>
2025-10-28 22:30:55 +0300

    Bir metin dizisi alın ve bu metinde kaç tane kelime olduğunu hesaplayan bir algoritma yazın.

---
+++ KelimeSayısıBulma/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Bir metin dizisi alın ve bu metinde kaç tane kelime olduğunu hesaplayan bir algoritma
+yazın.
+
+1)Başla
+2)Kullanıcıdan bir metin al
+3)Metni metin adında bir değişkene ata
+4)metin değişkenini split metoduyla ayır ve kelimeler adında yeni bir diziye ata
+5)dizinin uzunluğunu yazdır
+6)bitir
+*/
+
+Console.WriteLine("bir metin giriniz");
+string metin = Console.ReadLine();
+
+string[] kelimeler = metin.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+Console.WriteLine($"kelime sayısı: {kelimeler.Length}");
+
