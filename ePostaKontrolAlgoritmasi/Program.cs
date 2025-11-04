07887e8ad355671f6cda25666427fcd3d494d772
Fatma Paltacı <fatmapaltaci34@gmail.com>
2025-11-05 00:08:25 +0300

    E posta ve şifre kontrol algortimaları

---
+++ ePostaKontrolAlgoritmasi/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ePostaKontrolAlgoritmasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen e posta adresinizi giriniz");
+            string eposta = Console.ReadLine();
+
+            if (eposta.Contains('@') && eposta.EndsWith(".com"))
+            {
+
+                Console.WriteLine($"e-posta adresiniz: {eposta}");
+            }
+            else
+            {
+                Console.WriteLine("geçersiz e_posta adresi");
+            }
+        }
+    }
+}
+
---
+++ sifreKontrolAlgoritmasi/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace sifreKontrolAlgoritmasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* şifreyi kabul edebilmesi için 8 karaktere eşit veya büyük olması gerekmektedir
+             * büyük harf, küçük harf ve rakam içermesi gerekmektedir
+             * özel karakter içermelidir
+             */
+
+            do
+            {
+                Console.WriteLine("Şifre oluşturunuz");
+                string password = Console.ReadLine();
+
+                if (password.Length < 8)
+                {
+                    Console.WriteLine("şifrenizin 8 veya daha fazla karakter olması gerekmektedir");
+                    continue;
+                }
+                bool hasUpper = false;
+                bool hasLower = false;
+                bool hasDigit = false;
+                bool hasSpecial = false;
+
+                for (int i = 0; i< password.Length; i++)
+                {
+                    if (password[i]>= 'A' && password[i]<= 'Z')
+                    {
+                        hasUpper = true;
+
+                    }else if(password[i]>= 'a' && password[i]<= 'z')
+                    {
+                        hasLower = true;
+
+                    }else if (password[i]>= '0' && password[i] <= '9')
+                    {
+                        hasDigit = true;
+                    }
+                    else
+                    {
+                        hasSpecial = true;
+                    }
+                }
+
+                if( hasUpper && hasLower && hasDigit && hasSpecial)
+                {
+                    Console.WriteLine($"şifreniz başarılı şekilde oluşturuldu, şifreniz : {password}");
+                    break;
+                
+                }else
+                {
+                    Console.WriteLine("şifrenizin 8 veya daha fazla karakter, en az bir büyük harf, bir küçük harf, bir özel karakter ve bir rakam içermesi gerekmektedir");
+                }
+
+            } while (true);
+
+            
+            
+        }
+    
+    }
+}
