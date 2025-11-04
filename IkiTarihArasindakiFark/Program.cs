4f13212faea11bf950aaa0a0d8be16fefd2765a6
Görkem Aydın <Grkem Aydn@DESKTOP-2JL53HM>
2025-11-04 23:19:50 +0300

    İki tarih arasındaki farkı hesaplayan algoritma

---
+++ IkiTarihArasindakiFark/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace IkiTarihArasindakiFark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             1.Basla
+             2.Başlangıç ve bitiş adında iki tarih değişkeni tanımla
+             3.Bitiştten başlangıç tarihini çıkar ve gün sayısı adında bir değişkene ata
+             4.Gün sayısı değişkenini ekrana yazdır
+             5.Bitir
+             */
+
+            DateTime baslangic = new DateTime(2024,3,20);
+            DateTime bitis = DateTime.Now;
+
+            int gunSayisi = (bitis - baslangic).Days;
+            Console.WriteLine($"İki tarih arasındaki gün sayısı : {gunSayisi}");
+
+        }
+    }
+}
