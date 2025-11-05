92c3cb946be373389ae079a82bea3a7b1265dfd5
Fatma Paltacı <fatmapaltaci34@gmail.com>
2025-11-05 22:21:28 +0300

    saniyeyi dijital saat formata cevirme algortiması

---
+++ saniyeyiDigitalSaateCevirmeAlgoritmasi/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace saniyeyiDigitalSaateCevirmeAlgoritmasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Saniye cinsinden bir süre girin: ");
+            int toplamSaniye = Convert.ToInt32(Console.ReadLine());
+
+            string saatFormati = SaniyeyiSaateCevir(toplamSaniye);
+
+            Console.WriteLine($"Dijital saat formatı: {saatFormati}");
+        }
+
+        static string SaniyeyiSaateCevir(int saniye)
+        {
+            int saat = saniye / 3600;
+            int dakika = (saniye % 3600) / 60;
+            int kalanSaniye = saniye % 60;
+
+            return $"{saat:D2}:{dakika:D2}:{kalanSaniye:D2}";
+        }
+    }
+}
