6446e44a0a7d7d73f44c4c819487bc7f4e354a06
bunyaminpilsim <bunyaminpilsim@gmail.com>
2025-11-05 21:25:53 +0300

    Fonksiyonlar

---
+++ fonksiyonlar/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace fonksiyonlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Fonksiyonlar
+            /* Fonksiyonlar, belirli bir görevi yerine getiren kod bloklarıdır.
+             Fonksiyonlar, kodunuzu daha düzenli, okunabilir ve yeniden kullanılabilir hale getirir.
+            Fonksiyon çeşitleri:
+            - Paramtere almayıp geriye değer döndürmyen fonksiyonlar
+            void fonksiyonAdi()
+            {
+                // Fonksiyonun yapacağı işlemler
+            }
+            void MerhabaDunyaYazdir()
+            {
+                Console.WriteLine("Merhaba Dünya!");
+            }
+            - Paramtere alıp geriye değer döndürmeyen fonksiyonlar
+            void fonksiyonAdi(int parametre1, string parametre2)
+            {
+                // Fonksiyonun yapacağı işlemler
+            }
+            void ToplaVeYazdir(int sayi1, int sayi2)
+            {
+                int toplam = sayi1 + sayi2;
+                Console.WriteLine("Toplam: " + toplam);
+            }
+
+            - parametre alamayıp geriye değer döndüren fonksiyonlar
+            dönüş tipi fonksiyonAdi()
+            {
+                // Fonksiyonun yapacağı işlemler
+                return dönüş tipi; // Örnek olarak bir değer döndürüyoruz
+            }
+            int GetRandomNumber()
+            {
+                Random rnd = new Random();
+                return rnd.Next(1, 100); // 1 ile 100 arasında rastgele bir sayı döndürüyor
+            }
+
+            - Paramtere alıp geriye değer döndüren fonksiyonlar
+            dönüş tipi fonksiyonAdi(int parametre1, string parametre2)
+            {
+                // Fonksiyonun yapacağı işlemler
+                return dönüş tipi; // Örnek olarak bir değer döndürüyoruz
+            }
+            int Topla(int sayi1, int sayi2)
+            {
+                return sayi1 + sayi2;
+            }
+            */
+            #endregion
+        }
+    }
+}
