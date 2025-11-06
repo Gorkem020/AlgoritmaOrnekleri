dd56aebb0b2e1ed10ca9a6a849e7a8eb2c743e0d
haticegit <haticegedik352@gmail.com>
2025-11-06 22:23:57 +0300

    Metindeki en uzun kelimeyi bulan algoritma

---
+++ MetindekiEnUzunKelime/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MetindekiEnUzunKelime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir metin giriniz");
+            string metin=Console.ReadLine();
+
+            Console.WriteLine( " metindeki en uzun kelime: "+EnUzunKelimeBul(metin));
+        }
+
+        static string EnUzunKelimeBul(string metin)
+        {
+            string[] kelimeler = metin.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string max = kelimeler[0];
+            for (int i = 1; i < kelimeler.Length; i++)
+            {
+
+                if (kelimeler[i].Length > max.Length)
+                {
+                    max = kelimeler[i];
+                }
+                
+
+            }
+            return max;
+        }
+    }
+}
