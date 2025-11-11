648073b6fa7a5f5229c216b6ec3a4f6c2a89d411
= <=>
2025-11-11 21:43:22 +0300

    Fibonacci dizisi ilk n terimini yazdıran algoritma

---
+++ FibonacciAlgoritmasi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    /*
+           başla
+           kullanıcdan terim sayısı al
+           ilk terim = 0 ikinci terim = 1 olarak tanımla 
+           ilk iki terimi yazdır
+           3 'ten terim sayısına kadar döngü başlat
+           döngü içine yeni terim tanımla ve ilk iki terimin toplamına ata
+           yeni terimi yazdır
+           ilk iki terimi kaydır
+           bitir
+
+       */
+    static void Main(string[] args)
+    {
+        Console.WriteLine("ilk kaç terimi almak istersin");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int birinciTerim = 0;
+        int ikinciTerim = 1;
+        Console.Write($"{birinciTerim} {ikinciTerim} ");
+        for(int i =3; i<=n ; i++)
+        {
+            int yeniTerim = birinciTerim + ikinciTerim;
+            Console.Write($"{yeniTerim} ");
+            birinciTerim = ikinciTerim;
+            ikinciTerim = yeniTerim;
+        }
+        
+        
+
+    }
+}
