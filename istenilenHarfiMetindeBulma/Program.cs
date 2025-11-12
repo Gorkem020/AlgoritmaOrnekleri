6d5b53b6e2fb7c25cf8e8ac4b47a28495dd45633
HabibeBudak <habibe.bdakk@gmail.com>
2025-11-12 22:41:23 +0300

    Kullanıcıdan bir harf alınmalı. Belirlenen cümlede o harf var ise harften sonrası ekrana yazılmamalı

---
+++ istenilenHarfiMetindeBulma/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace istenilenHarfiMetindeBulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1.BASLA
+             * 2.Kullanıcıdan cümleyi al
+             * 3.Kullanıcıdan harfi al
+             * 4. Harfin cümledeki ilk geçtiği yeri bul
+             * 5.Harften öncesini al
+             * 6. Ekrana yazdır
+             * 7. Bitir
+             * 
+             */
+
+            Console.Write("Bir cümle girin: ");
+            string cumle = Console.ReadLine().ToLower();
+            
+            Console.Write("Bir harf girin: ");
+            char harf = Console.ReadKey().KeyChar;
+            harf = char.ToLower(harf);
+
+            Console.WriteLine(); 
+           
+            int index = cumle.IndexOf(harf);
+
+            if (index != -1)
+            {
+                
+                string sonuc = cumle.Substring(0, index+1);
+                Console.WriteLine("Sonuç: " + sonuc);
+            }
+            else
+            {
+                Console.WriteLine("Girilen harf cümlede bulunamadı.");
+            }
+        }
+    }
+}
